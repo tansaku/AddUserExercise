867e11d4805ced43d88e5697c9b1a80cb06ea559
Sam Joseph <tansaku@gmail.com>
2013-12-06 13:35:02 +0000

    adding form and testing for submission

--- features/add_user.feature
+++ features/add_user.feature
@@ -7,4 +7,6 @@ Feature: Admin adds user
   	Given I am on the home page
 	And I click "Add user"
     Then I should be on the new user page
-		And I fill in "Username" with "Jon" and "Password" with "Password"
+	And I fill in "Username" with "Jon" and "Password" with "Password"
+	And I press "Create User"
+	Then I should see "New User: Jon"
--- features/step_definitions.rb
+++ features/step_definitions.rb
@@ -13,7 +13,15 @@ Then(/^I should be on the new user page$/) do
   page.should have_content 'Password: '
 end
 
-Then(/^I fill in "(.*?)" with "(.*?)" and "(.*?)" with "(.*?)"$/) do |arg1, arg2, arg3, arg4|
-	fill_in arg1, :with => arg2
-	fill_in arg3, :with => arg4
+Then(/^I fill in "(.*?)" with "(.*?)" and "(.*?)" with "(.*?)"$/) do |field1, text1, field2, text2|
+	fill_in field1, :with => text1
+	fill_in field2, :with => text2
+end
+
+Then(/^I press "(.*?)"$/) do |button|
+  click_button button
+end
+
+Then(/^I should see "(.*?)"$/) do |text|
+  page.should have_content text
 end
